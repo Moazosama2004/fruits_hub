36fe0025d65a6765a314b0b2edb4372de87e88b7
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-12 15:27:26 +0200

    finish splash ui design and add assets folder

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruits_hub/core/utils/app_assets.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: SvgPicture.asset(Assets.imagesPlant),
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.cover),
+      ],
+    );
   }
 }
