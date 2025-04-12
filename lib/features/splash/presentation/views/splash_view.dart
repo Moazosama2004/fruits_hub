226690f03812c1be80de41ea8029a4a179b83692
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-12 15:35:10 +0200

    splash view feature done

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/onboarding/presentation/views/on_boarding_view.dart';
 import 'package:fruits_hub/features/splash/presentation/views/widgets/splash_view_body.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
-  static const  routeName = 'splashView';
+  static const routeName = 'splashView';
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: ,
       body: SplashViewBody(),
+    );
+  }
+
+  void excuteNavigation() {
+    Future.delayed(
+      Duration(seconds: 5),
+      () =>
+          Navigator.of(context).pushReplacementNamed(OnBoardingView.routeName),
     );
   }
 }
