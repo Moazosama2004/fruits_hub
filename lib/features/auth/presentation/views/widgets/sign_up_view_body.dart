ff25a62bd704a0230e44f31dadb6e9372c51dfc0
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-15 17:55:16 +0200

    refactor code

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,11 +1,9 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/constants.dart';
-import 'package:fruits_hub/core/utils/app_colors.dart';
-import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
-import 'package:fruits_hub/features/auth/presentation/views/widgets/custom_check_box.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/already_have_an_account.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/terms_and_conditions.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
@@ -46,89 +44,6 @@ class SignUpViewBody extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class AlreadyHaveAnAccount extends StatelessWidget {
-  const AlreadyHaveAnAccount({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        children: [
-          TextSpan(
-            text: 'تمتلك حساب بالفعل؟',
-            style: AppTextStyles.semiBold16.copyWith(
-              color: const Color(0xff949D9E),
-            ),
-          ),
-          TextSpan(
-            recognizer:
-                TapGestureRecognizer()
-                  ..onTap = () {
-                    Navigator.pop(context);
-                  },
-            text: ' تسجيل دخول',
-            style: AppTextStyles.semiBold16.copyWith(
-              color: AppColors.primaryColor,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TermsAndConditions extends StatefulWidget {
-  const TermsAndConditions({super.key});
-
-  @override
-  State<TermsAndConditions> createState() => _TermsAndConditionsState();
-}
-
-class _TermsAndConditionsState extends State<TermsAndConditions> {
-  bool isTermsAccepted = false;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomCheckBox(
-          isChecked: isTermsAccepted,
-          onChecked: (bool value) {
-            isTermsAccepted = value;
-            setState(() {});
-          },
-        ),
-        const SizedBox(width: 16),
-        Expanded(
-          child: Text.rich(
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: 'من خلال إنشاء حساب ، فإنك توافق على ',
-                  style: AppTextStyles.semiBold13.copyWith(
-                    color: const Color(0xff949D9E),
-                  ),
-                ),
-                TextSpan(
-                  recognizer:
-                      TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.pop(context);
-                        },
-                  text: ' الشروط والأحكام الخاصة بنا',
-                  style: AppTextStyles.semiBold13.copyWith(
-                    color: AppColors.lightPrimaryColor,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
