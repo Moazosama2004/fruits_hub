3a0402a41df2c27086c6f055cd5f7906193dfc67
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-17 18:54:40 +0200

    create firebase auth service Implement create user with email and password

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,44 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/errors/exceptions.dart';
+
+class FirebaseAuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      log(
+        "Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()} and code is ${e.code}",
+      );
+      if (e.code == 'weak-password') {
+        throw CustomException(errMessage: 'الرقم السري ضعيف جداً.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(
+          errMessage: 'لقد قمت بالتسجيل مسبقاً. الرجاء تسجيل الدخول.',
+        );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(errMessage: 'تاكد من اتصالك بالانترنت.');
+      } else {
+        throw CustomException(
+          errMessage: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+        );
+      }
+    } catch (e) {
+      log(
+        "Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()}",
+      );
+
+      throw CustomException(
+        errMessage: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+      );
+    }
+  }
+}
