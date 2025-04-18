e1bcaa332db2f5e242fa5f2bf934224c104910cb
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-19 01:56:14 +0200

    configure facebook and google with add logic for both

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,7 +1,9 @@
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fruits_hub/errors/exceptions.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseAuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -39,5 +41,80 @@ class FirebaseAuthService {
         errMessage: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
       );
     }
+  }
+
+  Future<User> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      log(
+        "Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()} and code is ${e.code}",
+      );
+      if (e.code == 'user-not-found') {
+        throw CustomException(
+          errMessage: 'الرقم السري او البريد الالكتروني غير صحيح.',
+        );
+      } else if (e.code == 'wrong-password') {
+        throw CustomException(
+          errMessage: 'الرقم السري او البريد الالكتروني غير صحيح.',
+        );
+      } else if (e.code == 'invalid-credential') {
+        throw CustomException(
+          errMessage: 'الرقم السري او البريد الالكتروني غير صحيح.',
+        );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(errMessage: 'تاكد من اتصالك بالانترنت.');
+      } else {
+        throw CustomException(
+          errMessage: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+        );
+      }
+    } catch (e) {
+      log(
+        "Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()}",
+      );
+      throw CustomException(
+        errMessage: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+      );
+    }
+  }
+
+  Future<User> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return (await _firebaseAuth.signInWithCredential(credential)).user!;
+  }
+
+  Future<User> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.tokenString);
+
+    // Once signed in, return the UserCredential
+    return (await _firebaseAuth.signInWithCredential(
+      facebookAuthCredential,
+    )).user!;
   }
 }
