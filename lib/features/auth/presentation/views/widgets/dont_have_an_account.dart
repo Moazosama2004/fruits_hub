098a3285c967935ab5272d22fea5b8bba90fa372
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-18 20:12:38 +0200

    edit in name

--- lib/features/auth/presentation/views/widgets/dont_have_an_account.dart
+++ lib/features/auth/presentation/views/widgets/dont_have_an_account.dart
@@ -2,7 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
-import 'package:fruits_hub/features/auth/presentation/views/sign_up_view.dart';
+import 'package:fruits_hub/features/auth/presentation/views/signup_view.dart';
 
 class DontHaveAnAccount extends StatelessWidget {
   const DontHaveAnAccount({super.key});
