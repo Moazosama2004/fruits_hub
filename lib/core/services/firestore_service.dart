e9ab4c39b9ec07bf8b492fc221f593c90db42d3b
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-19 18:05:35 +0200

    implement add data method

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,1 +1,14 @@
-class FirestoreService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruits_hub/core/services/database_service.dart';
+
+class FirestoreService implements DatabaseService {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    await _firestore.collection(path).add(data);
+  }
+}
