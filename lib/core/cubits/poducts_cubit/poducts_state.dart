8120baf64737fc99a321352cc562b8a0af4b9eb1
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-23 03:39:29 +0200

    create products cubit

---
+++ lib/core/cubits/poducts_cubit/poducts_state.dart
@@ -0,0 +1,20 @@
+part of 'poducts_cubit.dart';
+
+@immutable
+sealed class ProductsState {}
+
+final class ProductsInitial extends ProductsState {}
+
+final class ProductsLoading extends ProductsState {}
+
+final class ProductsFailure extends ProductsState {
+  final String errMessage;
+
+  ProductsFailure(this.errMessage);
+}
+
+final class ProductsSuccess extends ProductsState {
+  final List<ProductEntity> products;
+
+  ProductsSuccess({required this.products});
+}
